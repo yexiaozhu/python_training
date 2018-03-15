89327c4ded111951e03f8d885bccb531e5893f60
yexiaozhu <557703988@139.com>
2018-03-15 11:59:52 +0800

    输入三个整数x,y,z，请把这三个数由小到大输出。

--- py_training.py
+++ py_training.py
@@ -2,30 +2,14 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 题目：输入某年某月某日，判断这一天是这一年的第几天？
-
-# 程序分析：以3月5日为例，应该先把前两个月的加起来，然后再加上5天即本年的第几天，特殊情况，
-# 闰年且输入月份大于2时需考虑多加一天：
-
-# year = int(input('year:\n'))
-# mouth = int(input('mouth:\n'))
-# day = int(input('day:\n'))
+# 题目：输入三个整数x,y,z，请把这三个数由小到大输出。
 #
-# mouths = (0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334)
-# if 0 < mouth <=12:
-#     sum = mouths[mouth -1]
-# else:
-#     print('data error')
-# sum += day
-# if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 !=0)):
-#     leap = 1
-# if (leap == 1) and (mouth > 2):
-#     sum += 1
-# print('it is the %dth day.' %sum)
+# 程序分析：我们想办法把最小的数放到x上，先将x与y进行比较，如果x>y则将x与y的值进行交换，然后再用x与z进行比较，
+# 如果x>z则将x与z的值进行交换，这样能使x最小。
 
-import time
-D = input('请输入年份，格式XXXX-XX-XX：')
-d = time.strptime(D, '%Y-%m-%d').tm_yday
-print(d)
-print(type(d))
-print('the {} day of this year!'.format(d))
+l = []
+for i in range(3):
+    x = int(input('输入第%d个整数：\n' %(i+1)))
+    l.append(x)
+l.sort()
+print(l)
