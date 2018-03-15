8e49fda2ebe3cc9b052a4835ad3f55cd73ac4a0d
yexiaozhu <557703988@139.com>
2018-03-15 14:17:14 +0800

    暂停1秒输出

--- py_training.py
+++ py_training.py
@@ -5,7 +5,12 @@
 # 题目：输出 9*9 乘法口诀表。
 #
 # 程序分析：分行与列考虑，共9行9列，i控制行，j控制列。
+# 题目：暂停一秒输出。
+#
+# 程序分析：使用 time 模块的 sleep() 函数。
+import time
 
 for i in range(1, 10):
     for j in range(1, i+1):
         print("%d*%d=%d" %(i, j, i*j))
+        time.sleep(1) # 暂停1秒
