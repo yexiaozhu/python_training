627eb002c4ba8f197b951145a05086838de63d4d
yexiaozhu <557703988@139.com>
2018-03-15 14:33:13 +0800

    暂停1秒输出，并格式化时间

--- py_training.py
+++ py_training.py
@@ -2,15 +2,12 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 题目：输出 9*9 乘法口诀表。
-#
-# 程序分析：分行与列考虑，共9行9列，i控制行，j控制列。
-# 题目：暂停一秒输出。
-#
-# 程序分析：使用 time 模块的 sleep() 函数。
-import time
+# 题目：暂停一秒输出，并格式化当前时间。
 
-for i in range(1, 10):
-    for j in range(1, i+1):
-        print("%d*%d=%d" %(i, j, i*j))
-        time.sleep(1) # 暂停1秒
+import time
+print(time.localtime())
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+
+time.sleep(1) # 暂停1秒
+
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
