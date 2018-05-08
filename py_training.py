9009a985e20fd8cce38997f0ccced846b050bdab
yexiaozhu <557703988@139.com>
2018-05-08 17:49:45 +0800

    正整数分解质因数

--- py_training.py
+++ py_training.py
@@ -2,13 +2,27 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 题目：打印出所有的"水仙花数"，所谓"水仙花数"是指一个三位数，其各位数字立方和等于该数本身。例如：153是一个"水仙花数"，因为153=1的三次方＋5的三次方＋3的三次方。
+# 题目：将一个正整数分解质因数。例如：输入90,打印出90=2*3*3*5。
 
-for number in range(100, 1000):
-    number_hundreds = int(number / 100)
-    number_ten = int(number / 10 % 10)
-    number_one = int(number % 10)
-    # print(number_one, number_ten, number_hundreds)
-    if number == number_hundreds ** 3 + number_ten ** 3 + number_one ** 3:
-        # print(number)
-        print('水仙花数', number)
+def reduceNum(n):
+    print('{} = '.format(n))
+    if not isinstance(n, int) or n <= 0:
+        print('请输入正确数字:')
+    elif n in [1]:
+        print('{}'.format(n))
+    while n not in [1]:#循环保证递归
+        for index in range(2, n+1):
+            if n % index == 0:
+                # print('index', index)
+                # print('n', n)
+                n = int(n/index)
+                # print('n', n)
+                if n == 1:
+                    print(index)
+                else:
+                    print('{} *'.format(index))
+                    break
+
+
+reduceNum(100)
+
