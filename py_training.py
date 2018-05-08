894bd369c7dd36145f01cb291f06390fca0d88dc
yexiaozhu <557703988@139.com>
2018-05-08 18:00:47 +0800

    条件嵌套计算

--- py_training.py
+++ py_training.py
@@ -2,27 +2,14 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 题目：将一个正整数分解质因数。例如：输入90,打印出90=2*3*3*5。
+# 题目：利用条件运算符的嵌套来完成此题：学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示。
 
-def reduceNum(n):
-    print('{} = '.format(n))
-    if not isinstance(n, int) or n <= 0:
-        print('请输入正确数字:')
-    elif n in [1]:
-        print('{}'.format(n))
-    while n not in [1]:#循环保证递归
-        for index in range(2, n+1):
-            if n % index == 0:
-                # print('index', index)
-                # print('n', n)
-                n = int(n/index)
-                # print('n', n)
-                if n == 1:
-                    print(index)
-                else:
-                    print('{} *'.format(index))
-                    break
+score = int(input('请输入成绩：'))
+if score >= 90:
+    grade = 'A'
+elif score >= 60:
+    grade = 'B'
+else:
+    grade = 'C'
 
-
-reduceNum(100)
-
+print('成绩：%d 属于 %s' % (score, grade))
