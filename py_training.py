81832ccaf7ff352e0767e48670d423ce9b7083fe
yexiaozhu <557703988@139.com>
2018-05-10 15:28:11 +0800

    20内阶乘之和

--- py_training.py
+++ py_training.py
@@ -2,15 +2,12 @@
 #coding = utf-8
 #author = yexiaozhu
 
-#题目：有一分数序列：2/1，3/2，5/3，8/5，13/8，21/13...求出这个数列的前20项之和。
-# 程序分析：请抓住分子与分母的变化规律。
+#题目：求1+2!+3!+...+20!的和。
+# 程序分析：此程序只是把累加变成了累乘。
 
-a = 2
-b = 1
-s = 0
-for i in range(0, 20):
-    num = a / b
-    s += num
-    a, b = a+b, a
-    print(num)
-print(s)
+sum = 0
+t = 1
+for i in range(1, 21):
+    t *= i
+    sum += t
+print('20内阶乘之和：%d' %sum)
