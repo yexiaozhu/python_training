7a2281f131ddf2da12c8cacd031ab7ce1b60cdac
yexiaozhu <557703988@139.com>
2018-05-10 14:05:03 +0800

    猴子吃桃问题

--- py_training.py
+++ py_training.py
@@ -2,22 +2,16 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 一球从100米高度自由落下，每次落地后反跳回原高度的一半；再落下，求它在第10次落地时，共经过多少米？第10次反弹多高？
-tour = []
-height = []
+#题目：猴子吃桃问题：猴子第一天摘下若干个桃子，当即吃了一半，还不瘾，又多吃了一个第二天早上又将剩下的桃子吃掉一半，又多吃了一个。以后每天早上都吃了前一天剩下的一半零一个。到第10天早上想再吃时，见只剩下一个桃子了。求第一天共摘了多少。
 
-hei = 100.0  # 起始高度
-tim = 10  # 次数
-
-for i in range(1, tim + 1):
-    # 从第二次开始，落地时的距离应该是反弹高度乘以2（弹到最高点再落下）
-    if i == 1:
-        tour.append(hei)
+x1 = 1
+l = []
+for i in range(10):
+    x2 = 2 * (x1 + 1)
+    if i == 0:
+        l.append(x1)
     else:
-        tour.append(2*hei)
-    hei /= 2
-    height.append(hei)
-print(tour)
-print(height)
-print('总高度：tour = {0}'.format(sum(tour)))
-print('第10次反弹高度：height = {0}'.format(height[-1]))
+        x1 = x2
+        l.append(x2)
+print(l)
+print(l[-1])gi
