ae6a64154905e31b4125d37803c3f883950f7857
yexiaozhu <557703988@139.com>
2018-05-10 13:34:37 +0800

    2到1000以内的完数

--- py_training.py
+++ py_training.py
@@ -2,17 +2,22 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 题目：求s=a+aa+aaa+aaaa+aa...a的值，其中a是一个数字。例如2+22+222+2222+22222(此时共有5个数相加)，几个数相加由键盘控制。
+# 一个数如果恰好等于它的因子之和，这个数就称为"完数"。例如6=1＋2＋3.编程找出1000以内的所有完数。
+from sys import stdout
 
-number = 0
-number_list = []
-n = int(input('n='))
-a = int(input('a='))
-for i in range(n):
-    number = number + a
-    a = a * 10
-    print(number)
-    number_list.append(number)
-
-number_total = sum(number_list)
-print('总和：%d' % number_total)
+for i in range(2, 1001):
+    k = []
+    n = -1
+    s = i
+    for j in range(1, i):
+        if i % j == 0:
+            n += 1
+            s -= j
+            k.append(j)
+    if s == 0:
+        print(i)
+        print(k)
+        for i in range(n+1):
+            # print(k[i])
+            stdout.write(str(k[i]))
+            stdout.write(' ')
