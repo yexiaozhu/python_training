4c14e8edadd169819e6b7837dc54379bbd935b41
yexiaozhu <557703988@139.com>
2018-05-14 17:02:00 +0800

    判断回文数字

--- py_training.py
+++ py_training.py
@@ -2,23 +2,17 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 题目：给一个不多于5位的正整数，要求：一、求它是几位数，二、逆序打印出各位数字。
-#
-# 程序分析：学会分解出每一位数。
-x = int(input("请输入一个数:\n"))
-a = int(x / 10000)
-b = int(x % 10000 / 1000)
-c = int(x % 1000 / 100)
-d = int(x % 100 / 10)
-e = int(x % 10)
+# 题目：一个5位数，判断它是不是回文数。即12321是回文数，个位与万位相同，十位与千位相同。
 
-if a != 0:
-    print("5 位数：", e, d, c, b, a)
-elif b != 0:
-    print("4 位数：", e, d, c, b,)
-elif c != 0:
-    print("3 位数：", e, d, c)
-elif d != 0:
-    print("2 位数：", e, d)
+a = int(input("请输入一个数字:\n"))
+x = str(a)
+flag = True
+
+for i in range(int(len(x) / 2)):
+    if x[i] != x[-i - 1]:
+        flag = False
+        break
+if flag:
+    print("%d 是一个回文数!" % a)
 else:
-    print("1 位数：", e)
+    print("%d 不是一个回文数!" % a)
