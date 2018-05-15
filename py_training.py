b95acf937050c8d475747dca1f5b17119b8eb4c0
yexiaozhu <557703988@139.com>
2018-05-15 17:57:06 +0800

    颜色输出（使用run运行）

--- py_training.py
+++ py_training.py
@@ -2,15 +2,18 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 题目：练习函数调用。
-def hello_world():
-    print('hello world')
+# 题目：文本颜色设置。
 
-
-def three_hellos():
-    for i in range(3):
-        hello_world()
-
-
-if __name__ == '__main__':
-    three_hellos()
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+# print(bcolors.WARNING)
+# print(bcolors.ENDC)
+print(bcolors.OKBLUE + "123444?" + bcolors.ENDC)
+# print('\033[1;31;40m')
