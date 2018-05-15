f164ba16efb2b1838b6c2a2c6e45f1b96a379728
yexiaozhu <557703988@139.com>
2018-05-15 14:58:32 +0800

    倒序排列

--- py_training.py
+++ py_training.py
@@ -2,38 +2,9 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 题目：请输入星期几的第一个字母来判断一下是星期几，如果第一个字母一样，则继续判断第二个字母。
-#
-# 程序分析：用情况语句比较好，如果第一个字母一样，则判断用情况语句或if语句判断第二个字母。。
+# 题目：按相反的顺序输出列表的值。
 
-s1 = input('请输入首字母：')
-if s1 == 'S':
-    s2 = input('请输入次字母：')
-    if s2 == 'a':
-        print('Saturday')
-    elif s2 == 'u':
-        print('Sunday')
-    else:
-        print('data error')
-
-elif s1 == 'F':
-    print('Friday')
-
-elif s1 == 'M':
-    print('Monday')
-
-elif s1 == 'T':
-    print('please input second letter')
-    s2 = input("please input:")
-
-    if s2 == 'u':
-        print('Tuesday')
-    elif s2 == 'h':
-        print('Thursday')
-    else:
-        print('data error')
-
-elif s1 == 'W':
-    print('Wednesday')
-else:
-    print('data error')
+a = ['one', 'two', 'three']
+print(a[::-1])
+for i in a[::-1]:
+    print(i)
