64c710ac47a31a9277ec5e46082e903981408486
wangjinzhu <wangjinzhu@ifunq.com>
2018-08-17 17:17:16 +0800

    数组插入新数值排序

--- py_training.py
+++ py_training.py
@@ -2,17 +2,23 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 题目：求一个3*3矩阵主对角线元素之和。
-# 程序分析：利用双重for循环控制输入二维数组，再将a[i][i]累加后输出。
+#  题目：有一个已经排好序的数组。现输入一个数，要求按原来的规律将它插入数组中。
+# 程序分析：首先判断此数是否大于最后一个数，然后再考虑插入中间的数的情况，插入后此元素之后的数，依次后移一个位置。
+
 if __name__ == '__main__':
-	a = []
-	sum = 0.0
-	for i in range(3):
-		# print('a:', a)
-		a.append([])
-		for j in range(3):
-			a[i].append(float(input("input num:\n")))
-	for k in range(3):
-		sum += a[k][k]
-	print('a:', a)
-	print('sum:', sum)
+	# 最后一位0作为占位符
+	a = [1,4,6,9,13,16,19,28,40,100,0]
+	print('原始a:', a)
+	for i in range(len(a)):
+		print(a[i])
+	number = int(input("\n插入一个数字；\n"))
+	end = a[-2]
+	if number > end:
+		a[-1] = number
+	else:
+		for i in range(len(a)):
+			if a[i] > number:
+				a.insert(i, number)
+				a.pop(-1)
+				break
+	print('排序后a', a)
