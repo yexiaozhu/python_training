ce45dd4e576bf4e2d35081a3179ea233d67ac4cc
wangjinzhu <wangjinzhu@ifunq.com>
2018-08-17 17:44:39 +0800

    数组逆序输出

--- py_training.py
+++ py_training.py
@@ -2,23 +2,13 @@
 #coding = utf-8
 #author = yexiaozhu
 
-#  题目：有一个已经排好序的数组。现输入一个数，要求按原来的规律将它插入数组中。
-# 程序分析：首先判断此数是否大于最后一个数，然后再考虑插入中间的数的情况，插入后此元素之后的数，依次后移一个位置。
-
+# 将一个数组逆序输出。
+# 程序分析：用第一个与最后一个交换。
+ 
 if __name__ == '__main__':
-	# 最后一位0作为占位符
-	a = [1,4,6,9,13,16,19,28,40,100,0]
-	print('原始a:', a)
-	for i in range(len(a)):
-		print(a[i])
-	number = int(input("\n插入一个数字；\n"))
-	end = a[-2]
-	if number > end:
-		a[-1] = number
-	else:
-		for i in range(len(a)):
-			if a[i] > number:
-				a.insert(i, number)
-				a.pop(-1)
-				break
-	print('排序后a', a)
+	a = [9,6,5,4,1]
+	N = len(a)
+	print(a)
+	for i in range(int(N / 2)):
+		a[i], a[N - i -1] = a[N - i - 1], a[i]
+	print(a)
