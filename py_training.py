31670e199d604fc7e324d843d692104c3b8d1f40
wangjinzhu <wangjinzhu@ifunq.com>
2018-08-21 16:18:12 +0800

    数字求平方

--- py_training.py
+++ py_training.py
@@ -2,9 +2,13 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 统计1到100之和
-
-tmp = 0
-for i in range(1, 101):
-    tmp += i
-print('The sum is %d' %tmp)
+# 求输入数字平方，平方结果小于50，程序停止
+def SQ(x):
+    return x * x
+print('如果平方结果小于50，程序停止')
+again = 1
+while again:
+    num = int(input('请输入一个数字：'))
+    print('运算结果：%d' %(SQ(num)))
+    if SQ(num) < 50:
+        again = 0
