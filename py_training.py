2c2cb37fcfce0ecdb15a8576e2b4726e6d422832
wangjinzhu <wangjinzhu@ifunq.com>
2018-08-21 14:54:01 +0800

    计算1到100之和

--- py_training.py
+++ py_training.py
@@ -2,22 +2,9 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 矩阵相加
+# 统计1到100之和
 
-X = [[12, 7, 3],
-     [4, 5, 6],
-     [7, 8, 9]]
-
-Y = [[5, 8, 1],
-     [6, 7, 3],
-     [4, 5, 9]]
-
-result = [[0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0]]
-
-for i in range(len(X)):
-    for j in range(len(X[0])):
-        result[i][j] = X[i][j] + Y[i][j]
-        
-print(result)
+tmp = 0
+for i in range(1, 101):
+    tmp += i
+print('The sum is %d' %tmp)
