27cf5190439de958e47fb4c97a241e83e850ff1c
wangjinzhu <wangjinzhu@ifunq.com>
2018-08-21 14:49:12 +0800

    矩阵相加

--- py_training.py
+++ py_training.py
@@ -2,20 +2,22 @@
 #coding = utf-8
 #author = yexiaozhu
 
-# 模仿静态变量(static)案例
-# 演示一个python作用域使用方法
+# 矩阵相加
 
-class Num:
-    nNum = 1
-    def inc(self):
-        self.nNum += 1
-        print('nNum = %d' %self.nNum)
+X = [[12, 7, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+Y = [[5, 8, 1],
+     [6, 7, 3],
+     [4, 5, 9]]
+
+result = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
+
+for i in range(len(X)):
+    for j in range(len(X[0])):
+        result[i][j] = X[i][j] + Y[i][j]
         
-if __name__ == "__main__":
-    nNum = 2
-    inst = Num()
-    for i in range(3):
-        nNum += 1
-        print('The num = %d' %nNum)
-        # inst = Num()
-        inst.inc()
+print(result)
